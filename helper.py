Fixed dimensionality of backprop.

helper.py
@@ -5,12 +5,13 @@ def sigma(x):
 
 
 def sigma_d(x):
-    s = sigam(x)
+    s = sigma(x)
     return s * (1 - s)
 
 
 def softmax(x):
     dot_exp = np.exp(x)
+    # print dot_exp.shape
     summed = np.sum(dot_exp, axis=1)
     summed = np.reshape(summed, (dot_exp.shape[0], 1))
     summed = np.repeat(summed, dot_exp.shape[1], axis=1)

hidden_to_output.py
@@ -37,14 +37,6 @@ class SoftmaxLayer(object):
         return (labels - pred)
 
     def update_weights(self, eta):
-        g = self.forward_prop(self.last_input, add_bias=False)
-        # print g.shape
         delta_k = self.get_delta_k()
-        print '---'
-        print self.weights.shape
-        print g.shape
-        print delta_k.shape
-        print eta
-        print '---'
-        self.weights = self.weights + eta * delta_k * g
+        self.weights = self.weights + eta * np.dot(np.transpose(self.last_input), delta_k)
         return self.weights

visible_to_hidden.py
@@ -30,16 +30,11 @@ class SigmoidLayer(object):
         wjk = SoftmaxLayer.weights
 
         # k = 10; wjk = 65*10
-        return sigma_d(aj) * np.dot(delta_k, np.transpose(wjk))
+
+        # Ignore the first column because it is bias.
+        return sigma_d(aj) * (np.dot(delta_k, np.transpose(wjk)))[:,1:]
 
     def update_weights(self, SoftmaxLayer, eta):
         delta_j = self.get_delta_j(SoftmaxLayer)
-        z_j = self.forward_prop(self.last_input, add_bias=False)
-        self.weights = self.weights + eta * delta_j * z_j * self.last_input
+        self.weights = self.weights + eta * np.dot(np.transpose(self.last_input), delta_j)
         return self.weights
-
-
-#    def back_prop(self, output_grad):
-#        # get_delta_k and get_output_weights come from michaels code
- #       output_grad = np.dot(get_delta_k, get_output_weights)
-#        return output_grad * sigma_d(self.last_input)